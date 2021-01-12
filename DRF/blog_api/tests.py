00684e56a452cbedfc3aeecc820d306ae15295f3
Taner Ahmed <tanerahmed87@gmail.com>
2021-01-12 11:01:10 +0200

    Build basic blog project with DRF and ReactJS

---
+++ DRF/blog_api/tests.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
+from django.contrib.auth.models import User
+
+
+class PostTest(APITestCase):
+
+    def test_view_post(self):
+        url = reverse('blog_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_post(self):
+        test_category = Category.objects.create(name='django')
+        testuser1 = User.objects.create_user(username='test_user1', password='123456789')
+        data = {'title': 'new', 'author': 1, 'excerpt': 'new', 'content': 'new', 'slug': 'test_slug'}
+        url = reverse('blog_api:listcreate')
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
---
+++ ReactJS/blogapi/src/components/PostLoading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PostLoading(Component) {
+	return function PostLoadingComponent({ isLoading, ...props }) {
+		if (!isLoading) {
+			console.log(props)
+			return <Component {...props} />;
+		}
+		return (
+			<p style={{ fontSize: '25px' }}>
+				We are waiting for the data to load!...
+			</p>
+		);
+	};
+}
+export default PostLoading;
